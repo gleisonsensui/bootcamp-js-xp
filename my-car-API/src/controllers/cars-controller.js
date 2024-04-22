a0a20b054ac9077593e062621e1c4c733ccb2a57
gleisonsensui <gleison.nsilva@hotmail.com>
2024-04-22 19:54:00 -0300

    created project.

---
+++ my-car-API/src/controllers/cars-controller.js
@@ -0,0 +1,36 @@
+import {promises as fs} from 'fs';
+
+async function dataShow() {
+
+    try {
+        fs.readFile("./src/data/car-list.json")
+    } catch (error) {
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
